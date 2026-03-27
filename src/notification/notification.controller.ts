168aa7225b93bff60a33afb0dd568e71ad60629f
biswojitbiswal <biswojitb474@gmail.com>
2026-03-27 04:32:15 +0000

    working on notification module

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post } from "@nestjs/common";
+import { Controller, Get, Param, Patch, Post } from "@nestjs/common";
 import { NotificationService } from "./notification.service";
+import { GetCurrentUserId } from "common/decorator/get-current-user-id.decorator";
 
 @Controller({
     path: 'notification',
@@ -8,5 +9,26 @@ import { NotificationService } from "./notification.service";
 export class NotificationController{
     constructor(private readonly notificationService: NotificationService){}
 
+    @Get()
+    async getByUserId(@GetCurrentUserId() userId: string){
+        return await this.notificationService.getByUserId(userId);
+    }
 
+
+    @Get('count')
+    async getUnreadCount(@GetCurrentUserId() userId: string){
+        return await this.notificationService.getUnreadCount(userId);
+    }
+
+
+    @Patch('bulk/mark-read')
+    async markAllAsRead(@GetCurrentUserId() userId: string){
+        return await this.notificationService.markAllAsRead(userId);
+    }
+
+
+    @Patch(':id/mark-read')
+    async markAsRead(@Param('id') id: string){
+        return await this.notificationService.markAsRead(id);
+    }
 }
--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -2,22 +2,24 @@ import { Injectable, NotFoundException } from "@nestjs/common";
 import { PrismaService } from "prisma/prisma.service";
 
 @Injectable({})
-export class NotificationService{
-    constructor(private readonly prisma: PrismaService){}
+export class NotificationService {
+    constructor(private readonly prisma: PrismaService) { }
 
 
-    async create(userId: string, title: string, message: string, type: string, metadata: any){
+    async create(data: {
+        userId: string
+        title: string
+        message: string
+        type: string
+        metadata?: any
+    }) {
         try {
             const notification = await this.prisma.notification.create({
                 data: {
-                    userId,
-                    title,
-                    message,
-                    type,
+                    ...data,
                     isRead: false,
-                    metadata
-                }
-            })
+                },
+            });
 
             return notification;
         } catch (error) {
@@ -26,17 +28,18 @@ export class NotificationService{
     }
 
 
-    async getByUserId(userId: string){
+    async getByUserId(userId: string) {
         try {
             const notifications = await this.prisma.notification.findMany({
                 where: {
                     userId,
                     isRead: false
                 },
+                orderBy: {
+                    createdAt: "desc",
+                },
                 take: 10
             })
-
-            if(!notifications) throw new NotFoundException("Notification not found");
 
             return notifications;
         } catch (error) {
@@ -45,25 +48,43 @@ export class NotificationService{
     }
 
 
-    async MarkAsRead(id: string){
+    async markAsRead(id: string) {
         try {
-            const notification = await this.prisma.notification.findUnique({
-                where: {id},
-                select: {
-                    id: true,
-                }
-            })
-
-            if(!notification) throw new NotFoundException("Notification not found");
-
             await this.prisma.notification.update({
-                where: {id: notification.id},
+                where: { id },
                 data: {
                     isRead: true
                 }
             })
 
-            return {message: "Notification marked as READ"}
+            return { message: "Notification marked as READ" }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+
+    async getUnreadCount(userId: string) {
+        try {
+            return await this.prisma.notification.count({
+                where: { userId }
+            })
+        } catch (error) {
+            throw error;
+        }
+    }
+
+
+    async markAllAsRead(userId: string){
+        try {
+            await this.prisma.notification.updateMany({
+                where: {userId},
+                data: {
+                    isRead: true
+                }
+            })
+
+            return {message: "Notifications are marked as READ"}
         } catch (error) {
             throw error;
         }
